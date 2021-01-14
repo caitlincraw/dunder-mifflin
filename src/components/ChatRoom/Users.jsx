38a923d8b1a28e2ef8c7d502ab63eebc8f2845e5
Caitlin Crawford <caitlin.crawford16@gmail.com>
2021-01-14 12:07:38 -0500

    Update socket connections

--- src/components/ChatRoom/Users.jsx
+++ src/components/ChatRoom/Users.jsx
@@ -6,7 +6,7 @@ function Users(props) {
   return (
     <div className="container u-container">
         <div className="title u-title">
-        <h4 className="title-name">Online Paper Lovers</h4>
+        <h4 className="title-name">Online Paper Lovers ({props.totalUsers})</h4>
         <button className="title-x" onClick={props.onClick}>X</button>
         </div>
         <div className="board u-board">
