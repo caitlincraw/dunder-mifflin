9b6b66ef76d6e9e8b7f859c0fdb3ee315e55abd8
Alexandra Kasper <alexandrakasper@alis-mbp.lan>
2021-01-18 08:52:08 -0500

    added some redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
