6a4c0eccf9d6e99590e76a44be94986ec64e7430
Caitlin Crawford <caitlin.crawford16@gmail.com>
2021-01-18 23:18:39 -0500

    Add sources and about me and update nav and footer

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,7 +1,171 @@
+import './AboutUs.css';
+import Ali from './images/ali.jpg';
+import Rudy from './images/rudy.jpg';
+import Caitlin from './images/caitlin.jpg';
+
+
 function AboutUs() {
     return (
-        <div className="view">
-            This is the About Us Page
+        <div className="view au-view">
+            <div className="myspace-card">
+                <h3>Ali Kasper</h3>
+                <p>"Motto goes here"</p>
+                <div className="myspace-header">
+                    <img className="headshot" src={Ali} alt="Ali"></img>
+                    <ul>
+                        <li>she/her</li>
+                        <li>PORTLAND, OR</li>
+                        <li>United States</li>
+                        <li><img src="https://www.myspacegens.com/images/online_now/131.gif" alt="myspace online now gif"></img></li>
+                    </ul> 
+                </div>
+                <div className="myspace-social">
+                    View My:
+                    <a href="https://github.com/alikasper" target="_blank" rel="noreferrer">Github</a> 
+                    |
+                    <a href="https://www.linkedin.com/in/ali-kasper/" target="_blank" rel="noreferrer">LinkedIn</a>
+                </div>
+                <div className="myspace-song">
+                    <h5>My Song of the Week</h5>
+
+                </div>
+                <div className="myspace-details">
+                <h5 className="table-header">Ali's Details</h5>
+                    <div className="details-table">
+                        <div className="row">
+                            <div className="title-column">
+                                Job Title
+                            </div>
+                            <div className="answer-column">
+                                Software Developer
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="title-column">
+                                Here For
+                            </div>
+                            <div className="answer-column">
+                                DC Bootcamp
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="title-column">
+                                Office Character
+                            </div>
+                            <div className="answer-column">
+                                TBD
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <div className="myspace-card">
+                <h3>Rudy Saavedra</h3>
+                <p>"Motto goes here"</p>
+                <div className="myspace-header">
+                    <img className="headshot" src={Rudy} alt="Rudy"></img>
+                    <ul>
+                        <li>he/him</li>
+                        <li>SAVANNAH, GA</li>
+                        <li>United States</li>
+                        <li><img src="https://www.myspacegens.com/images/online_now/131.gif" alt="myspace online now gif"></img></li>
+                    </ul> 
+                </div>
+                <div className="myspace-social">
+                    View My:
+                    <a href="https://github.com/RudyCruisin" target="_blank" rel="noreferrer">Github</a> 
+                    |
+                    <a href="https://www.linkedin.com/in/rudysa2/" target="_blank" rel="noreferrer">LinkedIn</a>
+                </div>
+                <div className="myspace-song">
+                    <h5>My Song of the Week</h5>
+                    
+                </div>
+                <div className="myspace-details">
+                <h5 className="table-header">Rudy's Details</h5>
+                    <div className="details-table">
+                        <div className="row">
+                            <div className="title-column">
+                                Job Title
+                            </div>
+                            <div className="answer-column">
+                                Software Developer
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="title-column">
+                                Here For
+                            </div>
+                            <div className="answer-column">
+                                DC Bootcamp
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="title-column">
+                                Office Character
+                            </div>
+                            <div className="answer-column">
+                                TBD
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <div className="myspace-card">
+                <h3>Caitlin Crawford</h3>
+                <p>"Motto goes here"</p>
+                <div className="myspace-header">
+                    <img className="headshot" src={Caitlin} alt="Caitlin"></img>
+                    <ul>
+                        <li>she/her</li>
+                        <li>CHATTANOOGA, TN</li>
+                        <li>United States</li>
+                        <li><img src="https://www.myspacegens.com/images/online_now/131.gif" alt="myspace online now gif"></img></li>
+                    </ul>
+                </div>
+                <div className="myspace-social">
+                    View My:
+                    <a href="https://github.com/caitlincraw" target="_blank" rel="noreferrer">Github</a> 
+                    |
+                    <a href="https://www.linkedin.com/in/caitlin-crawford-aicp-6716778a/" target="_blank" rel="noreferrer">LinkedIn</a>
+                </div>
+                <div className="myspace-song">
+                    <h5>My Song of the Week</h5>
+
+                    
+                </div>
+                <div className="myspace-details">
+                    <h5 className="table-header">Caitlin's Details</h5>
+                    <div className="details-table">
+                        <div className="row">
+                            <div className="title-column">
+                                Job Title
+                            </div>
+                            <div className="answer-column">
+                                Software Developer
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="title-column">
+                                Here For
+                            </div>
+                            <div className="answer-column">
+                                DC Bootcamp
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="title-column">
+                                Office Character
+                            </div>
+                            <div className="answer-column">
+                                TBD
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     )
--- src/components/Navigation/Footer.jsx
+++ src/components/Navigation/Footer.jsx
@@ -4,7 +4,11 @@ import { Link } from 'react-router-dom';
  const Footer = () => {
     return(
             <footer>
+                <Link to="/sources">Sources</Link>
                 <Link to="/aboutus">About The Developers</Link>
+                <div className="copyright">
+                    Copyright &copy; {new Date().getFullYear()} Dunder Mifflin
+                </div>
             </footer>  
     )
 }
