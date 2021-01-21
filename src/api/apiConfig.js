ef05ac45bdbc0c771126ce4c6060b463142b1f82
Caitlin Crawford <caitlin.crawford16@gmail.com>
2021-01-20 19:00:05 -0500

    Fix api call

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
-const { REACT_APP_PROTOCOL, REACT_APP_HOST, REACT_APP_PORT } = process.env;
+const { REACT_APP_PROTOCOL, REACT_APP_SERVER_HOST } = process.env;
 
 export function getBackendUrl() {
-    return `${REACT_APP_PROTOCOL}://${REACT_APP_HOST}:${REACT_APP_PORT}`
+    return `${REACT_APP_PROTOCOL}://${REACT_APP_SERVER_HOST}`
     // return "http://localhost:1725"
 }
