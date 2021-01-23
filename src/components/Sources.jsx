d42e4506937554a8a1b4f1ee04d99e8fbb5e231d
Caitlin Crawford <caitlin.crawford16@gmail.com>
2021-01-23 13:43:50 -0500

    Update sources page content

--- src/components/Sources.jsx
+++ src/components/Sources.jsx
@@ -10,11 +10,16 @@ function Sources() {
                 <div className="sources-board">
                     <ul>
                         <li>The original Dundler Mifflin Infinity website was pitched in episodes 3 and 4 of the fourth season of the Office.</li>
-                        <li>Mooing sound by <a href="https://freesound.org/people/toddtruax/sounds/144820/" target="_blank" rel="noreferrer" >Todd Truax</a></li>
-                        <li>Phone ringing sound by <a href="https://freesound.org/people/elijahgoodson/sounds/411581/" target="_blank" rel="noreferrer" >Elijah Goodson</a></li>
-                        <li>Cat meowing sound by <a href="https://freesound.org/people/Taira%20Komori/sounds/211688/" target="_blank" rel="noreferrer" >Taira Komori</a></li>
-                        <li>Door opening sound by <a href="https://freesound.org/people/Cepagrave/sounds/476911/" target="_blank" rel="noreferrer" >Cepagrave</a></li>
-                        <li>Door closing sound by <a href="https://freesound.org/people/CGEffex/sounds/86985/" target="_blank" rel="noreferrer" >CG Effex</a></li>
+                        <br />
+                        <li>All sounds used in the chatroom were sourced from freesound.org
+                            <ul>
+                                <li>Mooing sound by <a href="https://freesound.org/people/toddtruax/sounds/144820/" target="_blank" rel="noreferrer" >Todd Truax</a></li>
+                                <li>Phone ringing sound by <a href="https://freesound.org/people/elijahgoodson/sounds/411581/" target="_blank" rel="noreferrer" >Elijah Goodson</a></li>
+                                <li>Cat meowing sound by <a href="https://freesound.org/people/Taira%20Komori/sounds/211688/" target="_blank" rel="noreferrer" >Taira Komori</a></li>
+                                <li>Door opening sound by <a href="https://freesound.org/people/Cepagrave/sounds/476911/" target="_blank" rel="noreferrer" >Cepagrave</a></li>
+                                <li>Door closing sound by <a href="https://freesound.org/people/CGEffex/sounds/86985/" target="_blank" rel="noreferrer" >CG Effex</a></li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
             </div>
