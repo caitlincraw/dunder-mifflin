eab30970aa60a9eeadef6dd4033303aa6f89291f
Caitlin Crawford <caitlin.crawford16@gmail.com>
2021-01-25 13:11:24 -0500

    Auto scroll functionality added

--- src/components/ChatRoom/Messages.jsx
+++ src/components/ChatRoom/Messages.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './ChatRoom.css';
 
-const Messages = ({ messages }) => (
-    <ul className="messages">
-        {messages.map((msg, index) => <li key={index}><b>{msg.user}:</b> {msg.message}</li>)}
-    </ul>
-);
+const Messages = ({ messages }) => {
+
+    const messagesEndRef = useRef(null)
+
+    const scrollToBottom = () => {
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+  
+    useEffect(() => {
+      scrollToBottom()
+    }, [messages]);
+
+    return (
+        <ul className="messages">
+            {messages.map((msg, index) => <li key={index}><b>{msg.user}:</b> {msg.message}</li>)}
+            <div ref={messagesEndRef} />
+        </ul>
+    )
+};
 
 export default Messages;
